dd7eb1d463b9f3116a92d87f0b0db1d3e9be023c
Tim <timrobertson0122@gmail.com>
2015-06-15 16:38:16 +0100

    added destroy with flash notice

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -60,4 +60,16 @@ feature 'restaurants' do
 
   end
 
+  context 'deleting restaurants' do 
+
+    before {Restaurant.create name: 'Trade'}
+
+    scenario 'removes a restaurant when a user clicks a delete link' do
+      visit '/restaurants'
+      click_link 'Delete Trade'
+      expect(page).not_to have_content 'Trade'
+      expect(page).to have_content 'Restaurant deleted successfully'
+    end
+  end
+
 end
