ce3aa41f93a32cddc1ad593e3605bfe20f62f7fb
Rodney Cullen <rodcul@gmail.com>
2015-06-16 09:55:38 +0100

    Adding validations - restaurant uniqueness

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -42,6 +42,11 @@ feature 'restaurants' do
       end
     end
 
+    it 'is not valid unless it has a unique name' do
+      Restaurant.create(name: "Moe's Tavern")
+      restaurant = Restaurant.new(name: "Moe's Tavern")
+      expect(restaurant).to have(1).error_on(:name)
+    end
   end
 
   context 'viewing restaurants' do
