f7a31d456de39131334311a6da9004d6e5b92a83
Rodney Cullen <rodcul@gmail.com>
2015-06-16 17:46:51 +0100

    Stage 2 setting limits on users - user can have reviewed restaurants test passing

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   has_many :restaurants, dependent: :destroy
+  has_many :reviews
+  has_many :reviewed_restaurants, through: :reviews, source: :restaurant
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
@@ -22,6 +24,10 @@ class User < ActiveRecord::Base
         user.email = data["email"] if user.email.blank?
       end
     end
+  end
+
+  def has_reviewed?(restaurant)
+    reviewed_restaurants.include? restaurant
   end
 
 end
