113971f9aa013a4afa0c855284ed36072e57af2c
Jennifer Arad-Marks <jlaradmarks@gmail.com>
2015-06-16 11:32:47 +0100

    new columns for Name and Image added to DB, self.from_omniauth method working

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150616093527) do
+ActiveRecord::Schema.define(version: 20150616103041) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,8 @@ ActiveRecord::Schema.define(version: 20150616093527) do
     t.datetime "updated_at",                          null: false
     t.string   "provider"
     t.string   "uid"
+    t.string   "name"
+    t.string   "image"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
