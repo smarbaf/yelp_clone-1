0b5ae22d75d3808f32f40886b91b2d546cfaf320
Rodney Cullen <rodcul@gmail.com>
2015-06-17 12:37:24 +0100

    User must login to add review

--- spec/features/reviews_feature_spec.rb
+++ spec/features/reviews_feature_spec.rb
@@ -16,4 +16,10 @@ feature 'reviewing' do
     expect(current_path).to eq '/restaurants'
     expect(page).to have_content('animal cruelty - stay away!')
   end
+
+  scenario 'user must log in to add a review' do
+    visit '/restaurants'
+    click_link 'Review KFC'
+    expect(page).to have_content('Log in')
+  end
 end
