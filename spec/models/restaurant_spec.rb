887e3c6c9900c95d0e9898a68f6e3f6f3ddb6928
DanielJohnston <macademy@danieljohnston.co.uk>
2015-06-18 17:16:38 +0100

    average actually calculated

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -29,3 +29,12 @@ context 'with 1 review' do
     expect(restaurant.average_rating).to eq(4)
   end
 end
+
+context 'multiple reviews' do
+  it 'returns the average' do
+    restaurant = Restaurant.create(name: 'The Ivy')
+    restaurant.reviews.create(rating: 1)
+    restaurant.reviews.create(rating: 5)
+    expect(restaurant.average_rating).to eq 3
+  end
+end
