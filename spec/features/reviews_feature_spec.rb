1b9bdd5b4e1ff0194314669ea72a899dc5f2394d
DanielJohnston <macademy@danieljohnston.co.uk>
2015-06-18 15:53:21 +0100

    failing tests for average review score

--- spec/features/reviews_feature_spec.rb
+++ spec/features/reviews_feature_spec.rb
@@ -4,9 +4,17 @@ include SessionHelpers
 
 feature 'reviewing' do
   before do
-      sign_up
-      create_restaurant('KFC')
-    end
+    sign_up
+    create_restaurant('KFC')
+  end
+
+  def leave_review(thoughts, rating)
+    visit '/restaurants'
+    click_link 'Review KFC'
+    fill_in 'Thoughts', with: thoughts
+    select rating, from: 'Rating'
+    click_button 'Leave Review'
+  end
 
   scenario 'allows users to leave a review using a form' do
     visit '/restaurants'
@@ -24,4 +32,14 @@ feature 'reviewing' do
     click_link 'Review KFC'
     expect(page).to have_content('Log in')
   end
+
+  scenario 'displays an average rating for all reviews' do
+    leave_review('So so', '3')
+    click_link 'Sign out'
+    sign_up 'fiona@mail.com'
+    visit '/restaurants'
+    leave_review('Magical wonderland', '5')
+    expect(page).to have_content('Average rating: 4')
+  end
+
 end
--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -6,7 +6,16 @@ describe Restaurant, :type => :model do
     expect(restaurant).to have(1).error_on(:name)
     expect(restaurant).not_to be_valid
   end
-end 
+end
 describe Restaurant, type: :model do
   it { is_expected.to have_many(:reviews).dependent(:destroy) }
-end  
+end
+
+describe '#average_rating' do
+  context 'no reviews' do
+    it 'returns "N/A" when there are no reviews' do
+      restaurant = Restaurant.create(name: 'The Ivy')
+      expect(restaurant.average_rating).to eq 'N/A'
+    end
+  end
+end
