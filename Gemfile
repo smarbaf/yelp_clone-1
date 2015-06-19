c763eb17603066c4f837e23e1b0dda0331847765
smarbaf <smarbaf@hotmail.com>
2015-06-19 14:30:37 +0100

    Adding js for incrementing endorsements

--- Gemfile
+++ Gemfile
@@ -53,4 +53,6 @@ group :test do
   gem 'capybara'
   gem 'shoulda'
   gem 'rspec-collection_matchers'
+  gem 'poltergeist'
+  gem 'database_cleaner'
 end
--- spec/features/endorsements_feature_spec.rb
+++ spec/features/endorsements_feature_spec.rb
@@ -6,11 +6,10 @@ feature 'endorsing reviews' do
     kfc.reviews.create(rating: 3, thoughts: 'It was an abomination')
   end
 
-  scenario 'a user can endorse a review, which updates the review endorsement count' do
+  scenario 'a user can endorse a review, which increments the endorsement count', js: true do
     sign_up 'fiona@mail.com'
     visit '/restaurants'
     click_link 'Endorse'
     expect(page).to have_content('1 endorsement')
-
   end
 end
